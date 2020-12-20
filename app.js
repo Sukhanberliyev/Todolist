47de9d725ca78f1dd0d083ecdc05f171089aef5e
Aidar Sukhanberliyev <sukhanberliev@gmail.com>
2020-12-20 14:58:02 +0900

    Add header and footer ejs and create new directory

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 let items = ["Buy food", "Cook food", "Eat food"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -20,15 +21,33 @@ app.get("/", (req,res) => {
 
   let day = today.toLocaleDateString("en-US", options)
 
-  res.render("list", {kindOfDay: day, newListItems: items});
+  res.render("list", {listTitle: day, newListItems: items});
 
 });
 
 app.post("/", (req,res) => {
+
   let item = req.body.newItem;
 
+  if (req.body.list === "Work"){
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
   items.push(item);
+  res.redirect("/");
+  }
+
 });
+
+app.get ("/work", (req,res) => {
+  res.render("list", {listTitle: "Work List", newListItems: workItems });
+});
+
+app.post("/work", (req,res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+})
 
 
 app.listen(3000, () => {
