049e59d3291beb8560fe996c959019df728a4c6c
Aidar Sukhanberliyev <sukhanberliev@gmail.com>
2020-12-20 14:11:58 +0900

    add style to the app

--- app.js
+++ app.js
@@ -2,29 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-var items = ["Buy food", "Cook food", "Eat food"];
+let items = ["Buy food", "Cook food", "Eat food"];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", (req,res) => {
 
-  var today = new Date();
+  let today = new Date();
 
-  var options = {
+  let options = {
     weekday: "long",
     day: "numeric",
     month: "long"
   };
 
-  var day = today.toLocaleDateString("en-US", options)
+  let day = today.toLocaleDateString("en-US", options)
 
   res.render("list", {kindOfDay: day, newListItems: items});
 
 });
 
 app.post("/", (req,res) => {
-  var item = req.body.newItem;
+  let item = req.body.newItem;
 
   items.push(item);
 });
