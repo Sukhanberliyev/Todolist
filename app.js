5995760f3ddbfbcbf753428e024c8e370738fb74
Aidar Sukhanberliyev <sukhanberliev@gmail.com>
2021-01-05 05:31:31 +0900

    add mongodb

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+
 
 const app = express();
 
@@ -10,18 +11,41 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true}, { useUnifiedTopology: true });
 
-app.get("/", function(req, res) {
+const itemsSchema = {
+  name: String
+};
 
-const day = date.getDate();
+const Item = mongoose.model("Item", itemsSchema);
 
-  res.render("list", {listTitle: day, newListItems: items});
+const item1 = new Item ({
+  name: "Welcome to your todolist!"
+});
+const item2 = new Item ({
+  name: "Hit the + button to add a new item"
+});
+const item3 = new Item ({
+  name: "<-- Hit this to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, (err) => {
+  if (err){
+    console.log(err);
+  } else {
+    console.log("Succesfully added to DB");
+  }
+})
+
+app.get("/", (req, res) => {
+
+  res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
-app.post("/", function(req, res){
+app.post("/", (req, res) => {
 
   const item = req.body.newItem;
 
@@ -34,11 +58,11 @@ app.post("/", function(req, res){
   }
 });
 
-app.get("/work", function(req,res){
+app.get("/work", (req,res) => {
   res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
-app.get("/about", function(req, res){
+app.get("/about", (req, res) => {
   res.render("about");
 });
 
